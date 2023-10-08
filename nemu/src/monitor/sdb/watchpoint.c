7100345a554d98c327e736a70625690e0745e905
tracer-ysyx <tracer@ysyx.org>
2023-10-08 18:36:24 +0800

    > compile NEMU ysyx_22040000 严森 Linux DESKTOP-RFMEKDK 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:36:24 up  2:37,  4 users,  load average: 0.14, 0.06, 0.01

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -71,7 +71,7 @@ void free_wp(int NO){
 }
 
 WP* check(uint32_t* new_result){//返回产生变化的变量
-	for(WP* it=head;it->next!=NULL;it=it->next){
+	for(WP* it=head;it!=NULL||it->next!=NULL;it=it->next){
 		bool success=true;
 		uint32_t result=expr(it->expr,&success);
 		if(result!=it->val) {
