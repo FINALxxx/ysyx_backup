5f838153fd1ef53067d4d83eef8b8701222e25d3
tracer-ysyx <tracer@ysyx.org>
2023-10-14 08:33:10 +0800

    > compile NEMU ysyx_22040000 严森 Linux DESKTOP-RFMEKDK 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  08:33:10 up  1:09,  3 users,  load average: 0.09, 0.04, 0.01

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -192,8 +192,8 @@ int op(int l,int r){
 			
 			//优先级表
 			if(type=='*'||type=='/') ptr_rank=1;
-			if(type=='+') ptr_rank=-1;//可以拓展减法 
-			if(type==PTR) ptr_rank=0;
+			if(type=='+') ptr_rank=0;//可以拓展减法 
+			if(type==PTR) ptr_rank=-1;
 
 			if(ptr_rank==main_op_rank){ 
 				main_op=MAX(main_op,ptr);//同等级：选择较后的op
