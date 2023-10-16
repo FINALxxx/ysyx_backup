ce9140a33ff38ac4cc847b488bc35099d9321ae5
tracer-ysyx <tracer@ysyx.org>
2023-10-16 23:46:59 +0800

    > compile NEMU ysyx_22040000 严森 Linux DESKTOP-RFMEKDK 5.15.90.1-microsoft-standard-WSL2 #1 SMP Fri Jan 27 02:56:13 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:46:59 up  2:26,  5 users,  load average: 0.04, 0.01, 0.00

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -32,7 +32,7 @@ enum {
 #define immI() do { *imm = SEXT(BITS(i, 31, 20), 12); } while(0)
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
-#define immJ() do { *imm = SEXT(BITS(i,31,12),32); } while(0)
+#define immJ() do { *imm = BITS(i, 20,20 ); } while(0)
 
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
@@ -45,7 +45,7 @@ static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_
     case TYPE_I: src1R();          immI(); break;
     case TYPE_U:                   immU(); break;
     case TYPE_S: src1R(); src2R(); immS(); break;
-	case TYPE_J:				   immJ(); break;
+	case TYPE_J:				   immJ();printf("%x",*imm); break;
   }
 }
 
