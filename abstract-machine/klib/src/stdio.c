3af11d764a1fdd33af77321a6c0d06e469d3a35e
tracer-ysyx <tracer@ysyx.org>
2023-11-17 21:18:42 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:18:42 up  7:22,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,45 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+	int cnt=0;
+	char* out_ptr = out;
+	while(*out_ptr!='\0') out_ptr++;
+	
+    va_list args=NULL;
+    va_start(args,fmt);
+    while(*fmt!='\0'){
+        switch(*fmt){
+            case '%':
+              	fmt++;
+                switch(*fmt){
+                	case 'c':
+						*out_ptr = (char)va_arg(args,int);
+						out_ptr++,cnt++;
+					break;
+					
+                    case 's':
+                    	strcat(out_ptr,va_arg(args,char*));
+                    	while(*out_ptr!='\0') out_ptr++,cnt++;
+					break;
+					
+					default://ERROR
+            			*out = ' ';
+            			out_ptr++,cnt++;
+					break;
+                }
+            break;
+              
+            default:
+            	*out_ptr = *fmt;
+            	out_ptr++,cnt++;
+			break;
+        }
+        fmt++; 
+    }
+    va_end(args);
+    *out_ptr='\0';
+    return cnt;
+
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
@@ -26,3 +64,4 @@ int vsnprintf(char *out, size_t n, const char *fmt, va_list ap) {
 }
 
 #endif
+
