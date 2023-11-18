c012455f002fa4f10690d378990d7172952d44d6
tracer-ysyx <tracer@ysyx.org>
2023-11-18 20:41:19 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:41:19 up  4:28,  2 users,  load average: 0.08, 0.02, 0.01

--- nemu/src/utils/log.c
+++ nemu/src/utils/log.c
@@ -24,10 +24,13 @@ void init_log(const char *log_file) {
 
   log_fp = stdout;
   if (log_file != NULL) {
+printf("%d\n",cnt);
+
     FILE *fp = fopen(log_file, "w");
     Assert(fp, "Can not open '%s'", log_file);
     log_fp = fp;
   }
+  printf("%d\n",cnt);
   Log("Log is written to %s", log_file ? log_file : "stdout");
 }
 
