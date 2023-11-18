90e9ef0b001ffdcd1a4b41e7db60495c2a984c68
tracer-ysyx <tracer@ysyx.org>
2023-11-18 16:37:23 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:37:23 up 24 min,  1 user,  load average: 0.00, 0.00, 0.00

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -21,10 +21,26 @@ int sprintf(char *out, const char *fmt, ...) {
     va_list args=NULL;
     va_start(args,fmt);
     while(*fmt!='\0'){
+		int int_val=0,int_cnt=0;
+		char int_reg[12]={'\0'};
         switch(*fmt){
             case '%':
               	fmt++;
                 switch(*fmt){
+					case 'd':
+						int_val = (int)va_arg(args,int);
+						if(int_val<0) *out_ptr='-',out_ptr++,int_val=-int_val;
+						for(;int_val;int_cnt++){
+							int_reg[int_cnt]=int_val%10;
+							int_val/=10;
+						}
+						cnt+=int_cnt;
+						for(int i=int_cnt-1;i>=0;i--){
+							*out_ptr = '0'+int_reg[i];
+							out_ptr++; 
+						}
+					break;
+
                 	case 'c':
 						*out_ptr = (char)va_arg(args,int);
 						out_ptr++,cnt++;
