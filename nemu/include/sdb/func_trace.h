0a16dca85da6e897e65b3d342ae9b6463566efc2
tracer-ysyx <tracer@ysyx.org>
2023-11-25 14:39:16 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:39:16 up  4:36,  3 users,  load average: 0.10, 0.05, 0.01

--- nemu/include/sdb/func_trace.h
+++ nemu/include/sdb/func_trace.h
@@ -5,21 +5,10 @@
 
 typedef struct {
 	char func_name[32];
-	Elf32_Addr start;
-	uint32_t size;
-} FuncInfo;
+	uint32_t start;
+	uint32_t end;
+} func;
 
-Elf32_Ehdr* read_elf_header(FILE* fp);
-void read_section(Elf32_Ehdr* elf_header,FILE* fp);
-uint32_t get_symtab(FILE* fp,Elf32_Shdr* SH_symtab,Elf32_Sym** symtab);
-void get_symtab_name(FILE* fp, Elf32_Sym* symtab,Elf32_Shdr* SH_strtab,uint32_t num_symtab_item);
-void parse_elf(const char* elf_file);
 
-/*
-typedef struct funcNode{
-	FuncInfo* func_src;//当前函数地址
-	FuncInfo* func_dst;//目标函数地址
-	int type;
-	funcNode* nxt; 
-} funcNode;
-*/
+void ftrace_init(FILE* fp);
+void parse_elf(const char* fileName);
