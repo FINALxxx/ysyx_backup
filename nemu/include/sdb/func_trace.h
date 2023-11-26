6c93b5817cadbb18d2665433a740403f6a92a928
finalx <3085378853@qq.com>
2023-11-26 11:41:16 +0800

    save

--- nemu/include/sdb/func_trace.h
+++ nemu/include/sdb/func_trace.h
@@ -5,21 +5,13 @@
 
 typedef struct {
 	char func_name[32];
-	Elf32_Addr start;
-	uint32_t size;
-} FuncInfo;
+	uint32_t start;
+	uint32_t end;
+} func;
 
-Elf32_Ehdr* read_elf_header(FILE* fp);
-void read_section(Elf32_Ehdr* elf_header,FILE* fp);
-uint32_t get_symtab(FILE* fp,Elf32_Shdr* SH_symtab,Elf32_Sym** symtab);
-void get_symtab_name(FILE* fp, Elf32_Sym* symtab,Elf32_Shdr* SH_strtab,uint32_t num_symtab_item);
-void parse_elf(const char* elf_file);
 
-/*
-typedef struct funcNode{
-	FuncInfo* func_src;//当前函数地址
-	FuncInfo* func_dst;//目标函数地址
-	int type;
-	funcNode* nxt; 
-} funcNode;
-*/
+void ftrace_init(FILE* fp);
+void parse_elf(const char* fileName);
+void fs_init();
+int32_t find_func(uint32_t pc);
+void call(uint32_t pc_src,uint32_t pc_dst,bool is_rst);
