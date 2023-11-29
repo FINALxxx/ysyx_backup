b234c9f235b56427b5ee1e63019b9c97bfd5743a
tracer-ysyx <tracer@ysyx.org>
2023-11-30 01:12:10 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:12:10 up  6:13,  4 users,  load average: 0.08, 0.02, 0.01

--- npc/csrc/read_am_bin.h
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <assert.h>
-#include <stdint.h>
-
-long read_init(FILE* fp,char const* fileName);
-
-uint32_t read_4_byte(FILE* fp);
-
-void read_total(FILE* fp,uint32_t* data,long cmd_num);	
