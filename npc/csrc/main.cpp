22ed7d43aa79bce5596e2f8bd3aebb09d41d4e6b
tracer-ysyx <tracer@ysyx.org>
2023-11-30 01:17:38 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:17:38 up  6:18,  4 users,  load average: 0.01, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -40,6 +40,7 @@ void sim_init(int argc,char** argv){
 	cmd_num = (fsize/4) + (fsize%4);
 	cout<<cmd_num<<endl;
 	cmd = (uint32_t*)malloc(cmd_num);
+	cout<<sizeof(cmd);
 	cout<<"(LOG)BIN FILE SIZE:"<<fsize<<endl;//读入bin文件
 
 	//env->traceEverOn(true);
