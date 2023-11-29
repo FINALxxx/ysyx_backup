afe59b292c7855ca9bd4b85088f6bf5a1e0cf52a
tracer-ysyx <tracer@ysyx.org>
2023-11-30 01:15:48 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:15:48 up  6:16,  4 users,  load average: 0.06, 0.01, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -69,8 +69,9 @@ extern "C" void halt(svBit is_dead){
 int main(int argc, char** argv) {
 	sim_init(argc,argv);
 	//read_total(fp,cmd,fsize/4,fsize%4);
+cout<<"TEST"<<endl;	
 	assert(fread(cmd,fsize,1,fp));
-	cout<<"TEST"<<endl;	
+	
 	while ( sim_time < MAX_SIM_TIME && cpu_status==ALIVE ) {
 		cpu->clk^=1;
 		cpu->rst=0;	
