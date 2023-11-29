eca3e7ce2fae54da2b166b6cb0fe51f934b779db
tracer-ysyx <tracer@ysyx.org>
2023-11-30 02:22:00 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  02:22:00 up  7:22,  3 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -77,7 +77,7 @@ int main(int argc, char** argv) {
 		//cpu->cmd=0b00000000000100001000000010010011;
 		cout<<"【CUR="<<cmd_cur<<"】"<<endl;
 		cpu->cmd=change_order(cmd[cmd_cur++]);
-		printf("【CMD=%x\n】",cpu->cmd);
+		printf("【CMD=%x】\n",cpu->cmd);
 		//if(sim_time==10) cpu->cmd=0b00000000000000000000000000000000;
 		cpu->eval();
 		sim_update();
