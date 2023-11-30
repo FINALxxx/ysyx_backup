f64ee3d26339a858d959b13a47a5f1abfe1df2b0
tracer-ysyx <tracer@ysyx.org>
2023-11-30 17:26:57 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:26:57 up  1:07,  4 users,  load average: 0.08, 0.02, 0.00

--- npc/obj_dir/Vcpu.mk
+++ npc/obj_dir/Vcpu.mk
@@ -35,7 +35,7 @@ VM_PREFIX = Vcpu
 VM_MODPREFIX = Vcpu
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-I csrc/include \
+	-Icsrc/include \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
