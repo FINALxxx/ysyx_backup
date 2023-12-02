0c0cf9e6103791b31c23bdeabdc7728da4b384cc
tracer-ysyx <tracer@ysyx.org>
2023-12-02 18:06:08 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:06:08 up  3:19,  2 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/PC.v
+++ npc/vsrc/PC.v
@@ -1,15 +1,27 @@
 module PC(
     input clk,
     input rst,
+    input a,
+    input b,
     output [31:0] npc
 );
 	wire reset;
     wire [31:0] tmp_pc,result;
-    assign result = tmp_pc + 32'd4;//可以之后实现一个小型adder
-    assign reset = rst;
+
+
+    adder add1(
+        .mode(0),
+        .a(a),
+        .b(b),
+        .S_U(1),
+        .result(result),
+        .LESS(),
+        .is_zero()
+    );
+
     Reg #(32,32'h80000000) pc2snpc (
         .clk(clk),
-        .rst(reset),
+        .rst(rst),
         .din(result),
         .dout(tmp_pc),
         .wen(1'b1)
