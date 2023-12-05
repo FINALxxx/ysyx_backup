091252ddc6d67b1813ea56b6965f8d78bbf3088a
tracer-ysyx <tracer@ysyx.org>
2023-12-05 23:28:03 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:28:03 up  2:20,  4 users,  load average: 0.00, 0.00, 0.00

--- npc/include/sys_npc.h
+++ npc/include/sys_npc.h
@@ -8,15 +8,15 @@
 #ifndef __NPC_SYS__
 #define __NPC_SYS__
 
-namespace sys_args{
-	vluint64_t sim_time=0;
-	FILE* fp =NULL;
-	uint32_t cmd_cur=0,cmd_num=0;
-	uint32_t* cmd=NULL;
+extern vluint64_t sim_time=0;
+extern FILE* fp =NULL;
+extern uint32_t cmd_cur=0,cmd_num=0;
+extern uint32_t* cmd=NULL;
 
-	VerilatedContext* env = NULL;
-	Vcpu* cpu = NULL;
-}
+extern VerilatedContext* env = NULL;
+extern Vcpu* cpu = NULL;
+
+
 //cpu运行参量，之后设置为static保护
 enum STATUS {DEAD,ALIVE,ABORT};//待添加
 STATUS cpu_status=ALIVE;
--- npc/main.cpp
+++ npc/main.cpp
@@ -2,12 +2,12 @@
 
 #define MAX_SIM_TIME 100
 
-using namespace sys_args;
+using namespace std;
 
 int main(int argc, char** argv) {
 	sim_init(argc,argv);
 	
-	while ( sys_args::sim_time < MAX_SIM_TIME && cpu_status==ALIVE) {
+	while ( sim_time < MAX_SIM_TIME && cpu_status==ALIVE) {
 		exec_once();
 		sim_update();
     }
