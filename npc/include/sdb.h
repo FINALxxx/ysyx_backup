06620600719daa3a26f2081d6bbba160804ee7ca
tracer-ysyx <tracer@ysyx.org>
2023-12-06 00:11:42 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:11:42 up  3:04,  4 users,  load average: 0.02, 0.02, 0.00

--- npc/include/sdb.h
+++ npc/include/sdb.h
@@ -28,15 +28,15 @@ static struct {
   const char *description;
   int (*handler) (char *);
 } cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "single-step execution", cmd_si},
-  { "info", "check the information of registers or watch points", cmd_info},
-  { "x", "visit the corresponding contents in memory", cmd_x },
-  { "p", "match the expr by regex", cmd_p},
-  { "w", "add a new watchpoint by using EXPR", cmd_w},
-  { "d", "delete a working watchpoint by using NO", cmd_d},
+  { "\033[0m\033[1;32mhelp\033[0m", "Display information about all supported commands", cmd_help },
+  { "\033[0m\033[1;32mc\033[0m", "Continue the execution of the program", cmd_c },
+  { "\033[0m\033[1;32mq\033[0m", "Exit NEMU", cmd_q },
+  { "\033[0m\033[1;32msi\033[0m", "single-step execution", cmd_si},
+  { "\033[0m\033[1;32minfo\033[0m", "check the information of registers or watch points", cmd_info},
+  { "\033[0m\033[1;32mx\033[0m", "visit the corresponding contents in memory", cmd_x },
+  { "\033[0m\033[1;32mp\033[0m", "match the expr by regex", cmd_p},
+  { "\033[0m\033[1;32mw\033[0m", "add a new watchpoint by using EXPR", cmd_w},
+  { "\033[0m\033[1;32md\033[0m", "delete a working watchpoint by using NO", cmd_d},
 };
 
 #define NR_CMD ARRLEN(cmd_table)
