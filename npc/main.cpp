202578991fe679376d91f83e785c729bcac16b3d
tracer-ysyx <tracer@ysyx.org>
2023-12-05 23:44:49 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:44:49 up  2:37,  4 users,  load average: 0.00, 0.00, 0.00

--- npc/main.cpp
+++ npc/main.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 int main(int argc, char** argv) {
 	sim_init(argc,argv);
-	
+	cout<<cpu_status<<endl;	
 	while ( sim_time < MAX_SIM_TIME && cpu_status==ALIVE) {
 		exec_once();
 		sim_update();
