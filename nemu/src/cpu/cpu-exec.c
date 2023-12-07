2f0662f2f4c8aff30bf45d8bd986e28f9e7ad446
tracer-ysyx <tracer@ysyx.org>
2023-12-07 17:33:30 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:33:30 up  1:31,  4 users,  load average: 0.01, 0.00, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -85,7 +85,7 @@ static void exec_once(Decode *s, vaddr_t pc) {
 
 #ifndef CONFIG_ISA_loongarch32r
   void disassemble(char *str, int size, uint64_t pc, uint8_t *code, int nbyte);
-  if(ilen!=4) printf("\n\nTEST=%ld\n\n",s->logbuf - p);
+  printf("\n\nTEST=%ld\n\n",s->logbuf - p);
   disassemble(p, s->logbuf + sizeof(s->logbuf) - p,
       MUXDEF(CONFIG_ISA_x86, s->snpc, s->pc), (uint8_t *)&s->isa.inst.val, ilen);
   insert_buffer(pc,p);//pa2.2:新增trace
