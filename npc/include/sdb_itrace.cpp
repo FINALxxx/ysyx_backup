c0f8aa9df8e6b16db2b5afe4a6a360cf1a81dba8
tracer-ysyx <tracer@ysyx.org>
2023-12-08 13:16:51 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:16:51 up  3:18,  5 users,  load average: 0.04, 0.01, 0.00

--- npc/include/sdb_itrace.cpp
+++ npc/include/sdb_itrace.cpp
@@ -10,7 +10,7 @@ bool is_full=false;//如果未满，就从0开始读取；如果满，就从cur+
 void cmd_disasm(uint32_t pc,char* disasm_rst){
 	//此处需要先转化为“以byte或uint8_t为单位”存储，对接llvm::arrayref的存储要求
 	uint8_t* cmd_src = (uint8_t*)cmd + pc_VtransP(pc);
-	printf("IN CMD_DISASM:%hhn\n",cmd_src);
+	printf("IN CMD_DISASM:%hhu\n",&cmd_src);
 	disassemble(disasm_rst,MAX_INST_LEN,pc,cmd_src,4);
 }
 
