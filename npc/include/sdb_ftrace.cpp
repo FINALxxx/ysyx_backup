9caee7700fee5bd04c8867e695e0d1e61ef00e0e
tracer-ysyx <tracer@ysyx.org>
2023-12-08 16:58:24 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:58:24 up  6:59,  4 users,  load average: 0.06, 0.02, 0.00

--- npc/include/sdb_ftrace.cpp
+++ npc/include/sdb_ftrace.cpp
@@ -76,7 +76,10 @@ void elf_init(const char* fileName){
 int32_t find_func(uint32_t pc){//注意，返回值是有符号的
 	printf("CPPTEST_FIND_PC=%x\n",pc);
 	for(int i=0;i<ptr;i++){
-		if(fs[i].start <= pc && pc <= fs[i].end) return i;//返回func_idx
+		if(fs[i].start <= pc && pc <= fs[i].end) {
+			printf("%d\n",i);
+			return i;//返回func_idx
+		}
 	}
 	return -1;
 }
