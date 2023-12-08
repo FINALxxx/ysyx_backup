12c833908f2a58bc6d03e4028308b458dbd89065
tracer-ysyx <tracer@ysyx.org>
2023-12-08 14:21:17 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:21:17 up  4:22,  4 users,  load average: 0.02, 0.03, 0.00

--- npc/include/sdb_itrace.cpp
+++ npc/include/sdb_itrace.cpp
@@ -35,7 +35,7 @@ void buffer_init(){
 }
 
 void buffer_disp(){
-	printf("\n\t[ITRACE RUNNING]\n");
+	printf("\n\t\033[0m\033[1;31m[ITRACE RUNNING]\033[0m\n");
 	uint32_t disp_ptr = is_full ? (cur+1)%MAX_BUF_SIZE : 0 ;//从0或者从cur+1开始
 	while(disp_ptr != cur-1){
 		printf("   %#10x:\t%-40s\t:%#010x\n",buf[disp_ptr].pc, buf[disp_ptr].log, buf[disp_ptr].inst);
