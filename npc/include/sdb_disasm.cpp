43314fa837894aa3c3f287000824f32feb46ce58
tracer-ysyx <tracer@ysyx.org>
2023-12-08 11:17:11 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:17:11 up  1:18,  3 users,  load average: 0.03, 0.03, 0.00

--- npc/include/sdb_disasm.cpp
+++ npc/include/sdb_disasm.cpp
@@ -12,6 +12,8 @@
 *
 * See the Mulan PSL v2 for more details.
 ***************************************************************************************/
+#include "sdb_disasm.h"
+
 
 #if defined(__GNUC__) && !defined(__clang__)
 #pragma GCC diagnostic push
