3fd3abd3e6f6172388c7cccc8f5ebc4b85d1a8dc
tracer-ysyx <tracer@ysyx.org>
2023-12-08 11:14:57 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  11:14:57 up  1:16,  3 users,  load average: 0.08, 0.04, 0.01

--- npc/include/sdb_itrace.cpp
+++ npc/include/sdb_itrace.cpp
@@ -7,7 +7,7 @@ traceNode buf[MAX_BUF_SIZE];
 uint32_t cur=0;
 bool is_full=false;//如果未满，就从0开始读取；如果满，就从cur+1开始读取。两者都一直读到cur-1为止
 
-void* cmd_disasm(uint32_t pc,char* disasm_rst){
+void cmd_disasm(uint32_t pc,char* disasm_rst){
 	//此处需要先转化为“以byte或uint8_t为单位”存储，对接llvm::arrayref的存储要求
 	uint8_t* cmd_src = (uint8_t*)cmd + pc_VtransP(pc);
 	printf("TEST:%hhn\n",cmd_src);
