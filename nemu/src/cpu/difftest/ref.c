74df4e778706c65e3dddb56940df362ce9a3c5c4
tracer-ysyx <tracer@ysyx.org>
2023-12-10 00:32:11 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:32:11 up 13:00,  4 users,  load average: 0.01, 0.02, 0.00

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -21,7 +21,7 @@
 
 //spike有类似的函数，在${HEMU_HOME}/tools/spike-diff/difftest.cc中
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction) {
-	//assert(0);
+	assert(0);
 	/*if(direction == DIFFTEST_TO_REF){
 		for(int i=0;i<n;++i){
 			paddr_write(addr+i,1, *((int8_t*)buf) );//写入1Byte数据
@@ -33,7 +33,7 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 }
 
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
-	//assert(0);
+	assert(0);
 	/*uint32_t reg_len = sizeof(cpu.gpr)/sizeof(cpu.gpr[0]);
 	if(direction == DIFFTEST_TO_REF){
 		for(int i=0;i<reg_len;i++){
@@ -48,7 +48,7 @@ __EXPORT void difftest_regcpy(void *dut, bool direction) {
 }
 
 __EXPORT void difftest_exec(uint64_t n) {
-  //assert(0);
+  assert(0);
   //cpu_exec(n);
 }
 
