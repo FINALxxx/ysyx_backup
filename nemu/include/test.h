d89a025fbc1e6325b2173ecbcc68a8dd217d7158
tracer-ysyx <tracer@ysyx.org>
2023-12-10 15:10:19 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:10:19 up  1:30,  5 users,  load average: 0.05, 0.03, 0.00

--- nemu/include/test.h
+++ nemu/include/test.h
@@ -1,4 +1,7 @@
-#include "difftest-def.h"
+#include <isa.h>
+#include <cpu/cpu.h>
+#include <difftest-def.h>
+#include <memory/paddr.h>
 
 __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction);
 
