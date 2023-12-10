114ee58c63969e5beae68b40c173648120dba2eb
tracer-ysyx <tracer@ysyx.org>
2023-12-10 14:59:31 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  14:59:31 up  1:19,  5 users,  load average: 0.02, 0.04, 0.01

--- npc/obj_dir/Vcpu.mk
+++ npc/obj_dir/Vcpu.mk
@@ -35,7 +35,7 @@ VM_PREFIX = Vcpu
 VM_MODPREFIX = Vcpu
 # User CFLAGS (from -CFLAGS on Verilator command line)
 VM_USER_CFLAGS = \
-	-I/home/finalx/ysyx-workbench/nemu/include/cpu \
+	-I/home/finalx/ysyx-workbench/nemu/include \
 
 # User LDLIBS (from -LDFLAGS on Verilator command line)
 VM_USER_LDLIBS = \
---
+++ npc/test.h
@@ -0,0 +1,9 @@
+__EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction);
+
+__EXPORT void difftest_regcpy(void *dut, bool direction);
+
+__EXPORT void difftest_exec(uint64_t n);
+
+__EXPORT void difftest_raise_intr(word_t NO);
+
+__EXPORT void difftest_init(int port);
