f4e4b7c91878dfe9a4c3a27befbbe8f2ab4b3b7c
tracer-ysyx <tracer@ysyx.org>
2023-12-13 23:02:15 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:02:15 up  5:59,  5 users,  load average: 0.00, 0.00, 0.00

--- npc/cinc/sys_npc.h
+++ npc/cinc/sys_npc.h
@@ -7,6 +7,7 @@
 #include <verilated_vcd_c.h>
 #include "Vcpu.h"
 #include <mem_config.h>
+#include <read_bin.h>
 
 #define MAX_SIM_TIME 10000
 #define NPCTRAP(PC_now,code) status_setter(DEAD,PC_now,code)
