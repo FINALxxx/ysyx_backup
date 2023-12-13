ad97cba2aa3caddf7a95ef73cfd40dce513dd83e
tracer-ysyx <tracer@ysyx.org>
2023-12-14 00:53:12 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:53:12 up  7:50,  4 users,  load average: 0.00, 0.00, 0.00

---
+++ npc/obj_dir/Vcpu__Dpi.h
@@ -0,0 +1,28 @@
+// Verilated -*- C++ -*-
+// DESCRIPTION: Verilator output: Prototypes for DPI import and export functions.
+//
+// Verilator includes this file in all generated .cpp files that use DPI functions.
+// Manually include this file where DPI .c import functions are declared to ensure
+// the C functions match the expectations of the DPI imports.
+
+#ifndef VERILATED_VCPU__DPI_H_
+#define VERILATED_VCPU__DPI_H_  // guard
+
+#include "svdpi.h"
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+
+    // DPI IMPORTS
+    // DPI import at /home/finalx/ysyx-workbench/npc/vsrc/cpu.v:2:35
+    extern svBitVecVal cmd_getter(const svBitVecVal* pc_now);
+    // DPI import at /home/finalx/ysyx-workbench/npc/vsrc/ControlUnit.v:3:30
+    extern void halt(svBit is_halt);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif  // guard
