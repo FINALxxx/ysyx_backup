39b5cb9344e2737704eccb04835603b5858eb5b1
tracer-ysyx <tracer@ysyx.org>
2023-12-13 17:57:49 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:57:49 up 54 min,  4 users,  load average: 0.00, 0.00, 0.00

--- npc/cinc/ifetch.h
+++ npc/cinc/ifetch.h
@@ -1,7 +1,7 @@
 #ifndef __CPU_IFETCH__
 #define __CPU_IFETCH__
 
-#include <memory/config.h>
+#include <config.h>
 
 static inline uword_t inst_fetch(vaddr_t pc, int size) {
   return vaddr_read(pc, size);
