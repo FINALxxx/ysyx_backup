559fe2dccabccef73232c35e67d7f59c88722f0a
tracer-ysyx <tracer@ysyx.org>
2023-12-14 01:30:57 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  01:30:57 up  8:27,  3 users,  load average: 0.00, 0.00, 0.00

--- npc/cinc/sys/cpu/inst_fetch.h
+++
@@ -1,4 +0,0 @@
-#ifndef __CPU_IFETCH__
-#define __CPU_IFETCH__
-
-#endif
--- npc/cinc/sys/init-load/read_bin.h
+++ npc/cinc/sys/init-load/read_bin.h
@@ -1,6 +1,6 @@
 #ifndef __INIT_READ__
 #define __INIT_READ__
-#include <sys_common.h>
+#include <common.h>
 
 void read_bin(const char* fileName);
 
