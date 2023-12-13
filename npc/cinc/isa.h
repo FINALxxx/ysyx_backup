8445169149aa9323d14085308a6398e0cbd84816
tracer-ysyx <tracer@ysyx.org>
2023-12-13 23:29:23 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:29:23 up  6:26,  5 users,  load average: 0.00, 0.01, 0.00

--- npc/cinc/isa.h
+++ npc/cinc/isa.h
@@ -3,9 +3,9 @@
 
 #include <isa-def.h>
 #include "Vcpu.h"
-extern NPC_STATUS cpu_status;
-extern long inst_exec_cnt;
-extern CPU_state cpu_data;
-extern Vcpu* cpu;
+//extern NPC_STATUS cpu_status;
+//extern long inst_exec_cnt;
+//extern CPU_state cpu_data;
+//extern Vcpu* cpu;
 
 #endif
