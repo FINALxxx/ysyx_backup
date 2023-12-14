000f891dad8dcf6171f01148c7372952755cd908
tracer-ysyx <tracer@ysyx.org>
2023-12-14 23:37:19 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  23:37:19 up 10:52,  4 users,  load average: 0.08, 0.02, 0.01

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,6 +1,6 @@
 #include <sys_npc.h>
 #include <cpu/exec.h>
-#include <sys/sys_npc.h>
+#include <cpu/status.h>
 //#include <sdb.h>
 
 
@@ -9,6 +9,6 @@ int main(int argc, char** argv) {
 	sim_init(argc,argv);
 	//exec(MAX_SIM_TIME);
 	//sdb_mainloop();
-	cpu=QUIT;	
+	cpu_status=QUIT;	
 	return is_exit_status_bad();
 }
