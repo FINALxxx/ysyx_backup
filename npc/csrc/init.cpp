48eca77f6b327934852daea4640230dabdaf2033
tracer-ysyx <tracer@ysyx.org>
2023-12-14 21:05:24 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  21:05:24 up  8:20,  5 users,  load average: 0.14, 0.05, 0.01

--- npc/csrc/init.cpp
+++ npc/csrc/init.cpp
@@ -1,8 +1,8 @@
-#include <isa.h>
 #include <memory/paddr.h>
 #include <verilated.h>
 #include "Vcpu.h"
 #include <verilated_vcd_c.h>
+#include <isa.h>
 
 static char* bin_file = NULL;//img文件
 static char* elf_file = NULL;//elf文件
