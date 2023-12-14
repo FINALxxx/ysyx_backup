604026b1e04653415159e5678d5583acaec42915
tracer-ysyx <tracer@ysyx.org>
2023-12-15 00:29:38 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:29:38 up 11:45,  5 users,  load average: 0.08, 0.02, 0.01

--- npc/csrc/sim-npc.cpp
+++ npc/csrc/sim-npc.cpp
@@ -8,6 +8,6 @@ int main(int argc,char** argv){
 	//for(int it = 0;it<argc;it++)
 	//	std::cout<<argv[it]<<std::endl;
 	init(argc,argv);
-	exec_once()	
+	exec_once();	
 	return is_exit_status_bad();
 }
