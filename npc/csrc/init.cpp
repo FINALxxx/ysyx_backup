af1fff00738190857f8d965f5cebc0ac26719dfe
tracer-ysyx <tracer@ysyx.org>
2023-12-14 18:40:38 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:40:38 up  5:56,  5 users,  load average: 0.07, 0.04, 0.01

--- npc/csrc/init.cpp
+++ npc/csrc/init.cpp
@@ -1,7 +1,7 @@
 #include <isa.h>
 #include <common.h>
 
-static void welcome(){
+void welcome(){
 	printf("welcome to %s-NPC\n",ANSI_FMT(CONFIG_ISA,ANSI_FG_YELLOW ANSI_BG_RED));
 
 }
--- npc/csrc/sim-npc.cpp
+++ npc/csrc/sim-npc.cpp
@@ -2,7 +2,7 @@
 
 
 int is_exit_status_bad();
-extern void welcome();
+void welcome();
 int main(int argc,char** argv){
 	//for(int it = 0;it<argc;it++)
 	//	std::cout<<argv[it]<<std::endl;
