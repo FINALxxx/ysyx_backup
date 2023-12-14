10225e205cdfbc733a2d18cc173d9aa4b362643e
tracer-ysyx <tracer@ysyx.org>
2023-12-14 13:25:29 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:25:29 up 40 min,  4 users,  load average: 0.00, 0.02, 0.00

--- npc/csrc/sim-npc.cpp
+++ npc/csrc/sim-npc.cpp
@@ -1,6 +1,6 @@
 #include <common.h>
 
 int main(int argc,char** argv){
-	for(char** it = argv;it!=NULL;it++)
-		std::cout<<*it<<std::endl;
+	//for(char** it = argv;it!=NULL;it++)
+		std::cout<<argv[0]<<std::endl;
 }
