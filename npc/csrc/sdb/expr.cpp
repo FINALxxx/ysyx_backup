6ea6dfed92bdc79a35505f9bbfb3b980dd7bdbec
tracer-ysyx <tracer@ysyx.org>
2023-12-15 16:16:36 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:16:36 up  6:20,  5 users,  load average: 0.02, 0.04, 0.00

--- npc/csrc/sdb/expr.cpp
+++ npc/csrc/sdb/expr.cpp
@@ -92,7 +92,7 @@ static bool make_token(char *e) {
     }
 
     if (i == NR_REGEX) {
-      printf("no match at position %d\n%s\n%*.s^\n", position, e, position, "");
+      Assert(0,"no match at position %d\n%s\n%*.s^\n", position, e, position, "");
       return false;
     }
   }
