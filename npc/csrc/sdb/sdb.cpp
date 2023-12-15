57089940798b3190c53a1d5427ae92e8ea1432a1
tracer-ysyx <tracer@ysyx.org>
2023-12-15 15:15:07 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:15:07 up  5:18,  5 users,  load average: 0.00, 0.02, 0.00

--- npc/csrc/sdb/sdb.cpp
+++ npc/csrc/sdb/sdb.cpp
@@ -102,6 +102,27 @@ static int cmd_d(char* args){
 
 }
 
+static int cmd_help(char *args);
+
+static struct { 
+  const char *name;
+  const char *description;
+  int (*handler) (char *);
+} cmd_table [] = {
+  { "help", "Display information about all supported commands", cmd_help },
+  { "c", "Continue the execution of the program", cmd_c },
+  { "q", "Exit NEMU", cmd_q },
+  { "si", "single-step execution", cmd_si},
+  { "info", "check the information of registers or watch points", cmd_info},
+  { "x", "visit the corresponding contents in memory", cmd_x },
+  { "p", "match the expr by regex", cmd_p},
+  { "w", "add a new watchpoint by using EXPR", cmd_w},
+  { "d", "delete a working watchpoint by using NO", cmd_d},
+};
+#define NR_CMD ARRLEN(cmd_table)
+
+
+
 static int cmd_help(char *args){
 	char *arg = strtok(NULL, " ");//mainloop中已经使用过一次strtok了，这里直接传NULL就能继续切割参数
 	int i;
@@ -127,22 +148,6 @@ static int cmd_help(char *args){
 	return 0;
 }
 
-static struct { 
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "single-step execution", cmd_si},
-  { "info", "check the information of registers or watch points", cmd_info},
-  { "x", "visit the corresponding contents in memory", cmd_x },
-  { "p", "match the expr by regex", cmd_p},
-  { "w", "add a new watchpoint by using EXPR", cmd_w},
-  { "d", "delete a working watchpoint by using NO", cmd_d},
-};
-#define NR_CMD ARRLEN(cmd_table)
 
 
 void sdb_init(){
