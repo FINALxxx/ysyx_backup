f66163a39cab63e06083b0d4b3692cb935fec178
tracer-ysyx <tracer@ysyx.org>
2023-12-15 15:57:08 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:57:08 up  6:00,  5 users,  load average: 0.16, 0.04, 0.01

--- npc/csrc/sim-npc.cpp
+++ npc/csrc/sim-npc.cpp
@@ -8,6 +8,7 @@ void engine_launch(int argc,char** argv);
 int is_exit_status_bad();
 
 int main(int argc,char** argv){
+	launch_mode = SDB;
 	engine_launch(argc,argv);
 	return is_exit_status_bad();
 }
