45cc776c99f450848f3c9f0f02e3f8bc1bdfc1ac
tracer-ysyx <tracer@ysyx.org>
2023-12-15 10:28:01 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:28:01 up 31 min,  5 users,  load average: 0.02, 0.02, 0.00

--- npc/cinc/std_sdb_init.h
+++ npc/cinc/std_sdb_init.h
@@ -3,10 +3,10 @@
 
 //两个init源文件都需要这4个变量
 //但是需要将两个源文件的变量进行隔离
-static char* bin_file;//img文件
-static char* elf_file;//elf文件
-static char* log_file;//log文件
-static char* diff_file;//diff动态链接库
+char* bin_file;//img文件
+char* elf_file;//elf文件
+char* log_file;//log文件
+char* diff_file;//diff动态链接库
 
 extern VerilatedContext* env;
 extern Vcpu* cpu;
