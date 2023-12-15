2195b15aa1cc743b59543171ca4e680f8031e7e9
tracer-ysyx <tracer@ysyx.org>
2023-12-15 15:08:42 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:08:42 up  5:12,  5 users,  load average: 0.01, 0.02, 0.00

--- npc/cinc/cpu.h
+++ npc/cinc/cpu.h
@@ -12,6 +12,8 @@ void set_cpu_status(int state, vaddr_t pc, int halt_ret);
 void get_cpu_pc();//之后加入DPIC
 word_t set_cpu_inst();//之后加入DPIC
 void get_cpu_reg();
+void reg_display();
+word_t reg_str2val(const char *s, bool *success);
 
 
 /* EXEC */
