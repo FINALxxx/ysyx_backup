63086921ba600212ba305c2e9269dcbefa3b3f3c
tracer-ysyx <tracer@ysyx.org>
2023-12-15 15:13:03 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  15:13:03 up  5:16,  5 users,  load average: 0.05, 0.04, 0.01

--- npc/cinc/sdb/sdb.h
+++ npc/cinc/sdb/sdb.h
@@ -4,21 +4,6 @@
 #include <readline/readline.h>
 #include <readline/history.h>
 
-static struct { 
-  const char *name;
-  const char *description;
-  int (*handler) (char *);
-} cmd_table [] = {
-  { "help", "Display information about all supported commands", cmd_help },
-  { "c", "Continue the execution of the program", cmd_c },
-  { "q", "Exit NEMU", cmd_q },
-  { "si", "single-step execution", cmd_si},
-  { "info", "check the information of registers or watch points", cmd_info},
-  { "x", "visit the corresponding contents in memory", cmd_x },
-  { "p", "match the expr by regex", cmd_p},
-  { "w", "add a new watchpoint by using EXPR", cmd_w},
-  { "d", "delete a working watchpoint by using NO", cmd_d},
-};
 
 #define NR_CMD ARRLEN(cmd_table)
 
