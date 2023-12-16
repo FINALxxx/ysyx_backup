1f4313c7e60b622a590fafe95f691cd99c8af6e9
tracer-ysyx <tracer@ysyx.org>
2023-12-16 09:21:50 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:21:50 up  2:29,  5 users,  load average: 0.06, 0.04, 0.00

--- npc/csrc/cpu.cpp
+++ npc/csrc/cpu.cpp
@@ -114,7 +114,7 @@ void exec_once(){
 	//cpu_data更新pc
 	get_cpu_pc();
 	//加载inst
-	IFDEF(CONFIG_STEP_PRINT, printf(ANSI_FMT("%#010x:\t%#010x\n",ANSI_FG_BLUE),cpu->pc,set_cpu_inst()), printf("TEST");set_cpu_inst());
+	IFDEF(CONFIG_STEP_PRINT, printf("TEST");printf(ANSI_FMT("%#010x:\t%#010x\n",ANSI_FG_BLUE),cpu->pc,set_cpu_inst()), set_cpu_inst());
 	//cpu_data更新inst
 	get_cpu_inst();
 	buffer_insert();
