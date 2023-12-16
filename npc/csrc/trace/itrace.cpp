aa0d7bcdb5dfb00eff732fb275af2c354396e0a7
tracer-ysyx <tracer@ysyx.org>
2023-12-16 16:21:27 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:21:27 up  9:28,  5 users,  load average: 0.06, 0.03, 0.00

--- npc/csrc/trace/itrace.cpp
+++ npc/csrc/trace/itrace.cpp
@@ -33,13 +33,13 @@ void buffer_init(){
 }
 
 void buffer_disp(){
-	printf("\t" ANSI_FMT("[ITRACE RUNNING]",ANSI_FG_GREEN,ANSI_BG_YELLOW)"\n");
+	printf("\t" ANSI_FMT("[ITRACE RUNNING]",ANSI_FG_GREEN ANSI_BG_YELLOW)"\n");
 	uint32_t disp_ptr = is_full ? (cur+1)%CONFIG_MAX_BUF_SIZE : 0 ;//从0或者从cur+1开始
 	while(disp_ptr != cur-1){
 		printf("   %#10x:\t%-40s\t:%#010x\n",buf[disp_ptr].pc, buf[disp_ptr].log, buf[disp_ptr].inst);
 		disp_ptr = (disp_ptr+1)%CONFIG_MAX_BUF_SIZE;
 	}
 	printf("-->%#10x:\t%-40s\t:%#010x\n",buf[cur-1].pc,buf[cur-1].log,buf[cur-1].inst);
-	printf("\t" ANSI_FMT("[ITRACE TERMINATE]",ANSI_FG_GREEN,ANSI_BG_YELLOW) "\n");
+	printf("\t" ANSI_FMT("[ITRACE TERMINATE]",ANSI_FG_GREEN ANSI_BG_YELLOW) "\n");
 }
 
