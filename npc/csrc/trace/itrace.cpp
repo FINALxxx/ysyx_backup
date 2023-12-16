bebe08d1bc710b3220fb24c9f5aa99d331af6579
tracer-ysyx <tracer@ysyx.org>
2023-12-16 10:03:29 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  10:03:29 up  3:10,  5 users,  load average: 0.00, 0.00, 0.00

--- npc/csrc/trace/itrace.cpp
+++ npc/csrc/trace/itrace.cpp
@@ -23,7 +23,7 @@ void buffer_insert(){
 	single_cmd_disasm(log);
 	strncpy(buf[cur].log, log, strlen(log));	
 	//使用LOG
-	Log("   %#10x:\t%-40s:%#010x\n",buf[cur].pc, buf[cur].log, buf[cur].inst);
+	Log("   %#10x:\t%s\t:%#010x\n",buf[cur].pc, buf[cur].log, buf[cur].inst);
 	cur = (cur+1)%CONFIG_MAX_BUF_SIZE;
 	is_full = is_full || (cur==CONFIG_MAX_BUF_SIZE-1);
 }
