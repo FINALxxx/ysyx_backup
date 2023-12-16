95f52d15baddb67882c69ce5592fb56c62532d6c
tracer-ysyx <tracer@ysyx.org>
2023-12-16 09:03:08 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:03:08 up  2:10,  5 users,  load average: 0.15, 0.07, 0.01

--- npc/cinc/sdb/watchpoint.h
+++ npc/cinc/sdb/watchpoint.h
@@ -2,16 +2,16 @@
 #define __SDB_WP_H__
 
 #include <common.h>
-typedef struct watchpoint {
+struct WP {
 	int NO;
 	struct watchpoint *next;
 	/* TODO: Add more members if necessary */
 	char expr_s[100];//表达式
 	uint32_t val;//值
 
-} WP;
-#define NR_WP 32
+};
 
+#define NR_WP 32
 
 WP* check_wp(uint32_t* new_result);
 void new_wp(char* expr_s);
