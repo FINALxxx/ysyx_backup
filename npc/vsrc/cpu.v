0b1a57efacec775cc2794141ca7a32a4437d45d4
tracer-ysyx <tracer@ysyx.org>
2023-12-17 16:24:40 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:24:40 up  8:30,  4 users,  load average: 0.05, 0.02, 0.00

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -146,6 +146,7 @@ module cpu(
 
 	//测试用，实现后一定要删除
     always @(*) begin
+		$display("result=%x",src_rd_ALU);
 		/*$display("cpu_dnpc=%x",dnpc);
 		$display("cmd=%x",cmd);   
 		//$display("clk=%b",clk);
