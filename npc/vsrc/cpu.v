12c58ab5a602b1ceddd9644fbd31dbd1a1b91665
tracer-ysyx <tracer@ysyx.org>
2023-12-17 17:32:44 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:32:44 up  9:38,  4 users,  load average: 0.00, 0.00, 0.00

--- npc/vsrc/cpu.v
+++ npc/vsrc/cpu.v
@@ -146,14 +146,14 @@ module cpu(
 
 	//测试用，实现后一定要删除
     always @(*) begin
-		/*$display("cpu_dnpc=%x",dnpc);
+		/*/*$display("cpu_dnpc=%x",dnpc);
 		$display("cmd=%x",cmd);   
 		//$display("clk=%b",clk);
 		//$display("pc=%x",pc);
 		$display("=========================================\n");
         $display("rs1=%b",rs1);
         $display("rs2=%b",rs2);
-        $display("rd=%b",rd);*/
+        $display("rd=%b",rd);
         $display("op-imm=%b",op_IMM);
 		$display("imm=%b\n\n",imm);
         $display("ALUsel=%b",op_ALU_sel);
@@ -163,14 +163,15 @@ module cpu(
 		//$display("a1=%b",a1);
         //$display("b1=%b",b1);
         $display("src_rd=%b",src_rd);
-        /*$display("PCAsrc=%b",op_PC_Asrc);
+        $display("PCAsrc=%b",op_PC_Asrc);
         $display("PCBsrc=%b",op_PC_Bsrc);
         $display("a0=%b",a0);
         $display("b0=%b",b0);
 		$display("is_load=%b",load);
 		$display("is_store=%b",store);
 		$display("is_pmem_valid=%b",en_PMEM);
-		$display("pmem_catgory=%b",op_PMEM);*/
+		$display("pmem_catgory=%b",op_PMEM);
+		*/
     end
 	
 
