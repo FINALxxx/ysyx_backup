e9b1bd63604f0076728f7d194990fe39c28bd569
tracer-ysyx <tracer@ysyx.org>
2023-12-17 09:40:14 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  09:40:14 up  1:45,  5 users,  load average: 0.06, 0.02, 0.00

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -99,7 +99,6 @@ static void execute(uint64_t n) {//如果传入很大的值n，for循环将执�
   Decode s;
   for (;n > 0; n --) { 
     exec_once(&s, cpu.pc);
-    printf("test\n");
 	g_nr_guest_inst ++; //记录客户指令的计数器
     //trace_and_difftest(&s, cpu.pc);
 	single_inst_debug(&s, cpu.pc);
