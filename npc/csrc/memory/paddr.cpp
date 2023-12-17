a60bc5afa0a2fc1dd612c651b28577b2a2f69b6d
tracer-ysyx <tracer@ysyx.org>
2023-12-17 17:43:07 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:43:07 up  9:48,  4 users,  load average: 0.02, 0.02, 0.00

--- npc/csrc/memory/paddr.cpp
+++ npc/csrc/memory/paddr.cpp
@@ -43,14 +43,14 @@ void paddr_write(paddr_t addr, int len, word_t data){
 
 extern "C" void pmem_read(int raddr, int *rdata) {
 	// 总是读取地址为`raddr & ~0x3u`的4字节返回给`rdata`
-	//printf("PREAD:%x\n",raddr);
+	printf("PREAD:%x\n",raddr);
 	*rdata = paddr_read(raddr,4);
 }
 extern "C" void pmem_write(int waddr, int wdata, char wmask) {
 	// 总是往地址为`waddr & ~0x3u`的4字节按写掩码`wmask`写入`wdata`
 	// `wmask`中每比特表示`wdata`中1个字节的掩码,
 	// 如`wmask = 0x3`代表只写入最低2个字节, 内存中的其它字节保持不变
-	//printf("PWRITE:%x,%x,%x\n",waddr,wdata,wmask);
+	printf("PWRITE:%x,%x,%x\n",waddr,wdata,wmask);
 	if(wmask == 0x1) paddr_write(waddr,1,wdata);
 	else if(wmask == 0x3) paddr_write(waddr,2,wdata);
 	else paddr_write(waddr,4,wdata);
