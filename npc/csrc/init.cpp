4766939800e584dac55e53963f4dfeada564f377
tracer-ysyx <tracer@ysyx.org>
2023-12-19 16:02:47 +0800

    > sim RTL ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:02:47 up  6:07,  6 users,  load average: 0.07, 0.03, 0.00

--- npc/csrc/init.cpp
+++ npc/csrc/init.cpp
@@ -116,7 +116,7 @@ void cpu_init(){
 	clk_update();
 	cpu->rst = 0;
 	clk_update();
-	half_clk_update();
+	clk_update();
 	set_cpu_inst();
 	printf(ANSI_FMT("PC_INIT",ANSI_FG_GREEN) ":" FMT_PADDR "\n",cpu->pc);
 	get_cpu_pc();
