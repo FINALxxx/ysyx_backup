69fc92da63541956796b6bcef37082d484d95105
tracer-ysyx <tracer@ysyx.org>
2023-12-20 19:56:20 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:56:20 up  8:05,  4 users,  load average: 0.17, 0.06, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -34,9 +34,10 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
               	fmt++;
                 switch(*fmt){
 					case 'd':
+						putch('d');
 						int_val = (int)va_arg(ap,int);
 						if(int_val<0) *out_ptr='-',out_ptr++,int_val=-int_val;
-						if(int_val==0) int_reg[0]=1,int_cnt++;
+						if(int_val==0) int_reg[0]=0,int_cnt++;
 						for(;int_val;int_cnt++){
 							int_reg[int_cnt]=int_val%10;
 							//putch('0'+int_reg[int_cnt]);
