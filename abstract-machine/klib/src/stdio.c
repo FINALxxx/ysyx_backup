a4d8e52218413e23406c858ed01bf0a4cb0cd068
tracer-ysyx <tracer@ysyx.org>
2023-12-20 18:46:00 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  18:46:00 up  6:54,  3 users,  load average: 0.08, 0.02, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -57,6 +57,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 					default://ERROR
             			*out = ' ';
             			out_ptr++,cnt++;
+						assert(0);
 					break;
                 }
             break;
