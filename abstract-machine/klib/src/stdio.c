df8798020a4743990744099c8011c93a1999d110
tracer-ysyx <tracer@ysyx.org>
2023-12-20 20:38:35 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:38:35 up  8:47,  4 users,  load average: 0.16, 0.06, 0.01

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -36,7 +36,7 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 					case 'd':
 						int_val = (long long)va_arg(ap,long long);
 						if(int_val<0) *out_ptr='-',out_ptr++,int_val=-int_val;
-						if(int_val==0) int_reg[0]=0,int_cnt++;
+						//if(int_val==0) int_reg[0]=0,int_cnt++;
 						for(;int_val;int_cnt++){
 							int_reg[int_cnt]=int_val%10;
 							//putch('0'+int_reg[int_cnt]);
