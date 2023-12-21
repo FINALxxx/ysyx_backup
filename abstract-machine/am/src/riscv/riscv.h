263b814c910a103a3b5d35ad1029f701cffd778e
tracer-ysyx <tracer@ysyx.org>
2023-12-21 16:12:14 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  16:12:14 up  5:11,  4 users,  load average: 0.00, 0.00, 0.00

--- abstract-machine/am/src/riscv/riscv.h
+++ abstract-machine/am/src/riscv/riscv.h
@@ -3,11 +3,15 @@
 
 #include <stdint.h>
 
-//接受一个ptr，取地址的1BYTE/HALF_WORD/WORD
+//接受一个uintptr_t(不是指针，是uint)
+//将uintptr_t转化为一个指针，并返回指针所指的内容
 static inline uint8_t  inb(uintptr_t addr) { return *(volatile uint8_t  *)addr; }
 static inline uint16_t inw(uintptr_t addr) { return *(volatile uint16_t *)addr; }
 static inline uint32_t inl(uintptr_t addr) { return *(volatile uint32_t *)addr; }
 
+
+//接受一个uintptr_t，将它转化为一个指针
+//接受不同长度（byte、half_word、word）的数据，并保存到指针所指的内容里
 static inline void outb(uintptr_t addr, uint8_t  data) { *(volatile uint8_t  *)addr = data; }
 static inline void outw(uintptr_t addr, uint16_t data) { *(volatile uint16_t *)addr = data; }
 static inline void outl(uintptr_t addr, uint32_t data) { *(volatile uint32_t *)addr = data; }
