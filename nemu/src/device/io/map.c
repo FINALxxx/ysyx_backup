a29f274b720f5b60b634b2d9d455fadf45a4f555
tracer-ysyx <tracer@ysyx.org>
2023-12-22 19:25:04 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  19:25:04 up  1:59,  5 users,  load average: 0.00, 0.00, 0.00

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -32,8 +32,10 @@ uint8_t* new_space(int size) {
   return p;
 }
 
+void disp_mem_buffer();
 static void check_bound(IOMap *map, paddr_t addr) {
   if (map == NULL) {
+	disp_mem_buffer();
     Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
   } else {
     Assert(addr <= map->high && addr >= map->low,
