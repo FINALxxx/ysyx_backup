85e95dee0740ac72d59939ac39ce43a97bb43b8b
tracer-ysyx <tracer@ysyx.org>
2023-12-22 22:08:24 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  22:08:24 up  4:42,  5 users,  load average: 0.05, 0.03, 0.00

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -131,8 +131,8 @@ void init_monitor(int argc, char *argv[]) {
   long img_size = load_img();
 
   /* Initialize differential testing. */
-  init_difftest(diff_so_file, img_size, difftest_port);
-
+  //init_difftest(diff_so_file, img_size, difftest_port);
+  printf("TEST:%ld\n",img_size);
   /* Initialize the simple debugger. */
   init_sdb();
 
