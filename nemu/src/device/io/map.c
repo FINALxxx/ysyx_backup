5828280e46eaa978c4a18692c7c3e78b0a5a2d7a
tracer-ysyx <tracer@ysyx.org>
2023-12-22 20:15:52 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:15:52 up  2:49,  5 users,  load average: 0.06, 0.02, 0.00

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -66,7 +66,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  printf("IN MAP_READ\n");
+  printf("IN MAP_WRITE\n");
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
