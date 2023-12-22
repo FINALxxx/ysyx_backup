ad84eeb86bc7756eebd35715ddffc096d855439a
tracer-ysyx <tracer@ysyx.org>
2023-12-23 00:10:21 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  00:10:21 up  6:44,  4 users,  load average: 0.08, 0.05, 0.08

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -85,7 +85,7 @@ static int parse_args(int argc, char *argv[]) {
     switch (o) {
       case 'b': sdb_set_batch_mode(); break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
-      case 'l': log_file = optarg;break;
+      case 'l': log_file = optarg;printf("TEST:%s\n",log_file);break;
       case 'd': diff_so_file = optarg; break;
 	  case 'e': 
 		#ifdef CONFIG_FTRACE
