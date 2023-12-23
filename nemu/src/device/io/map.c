df2c96b8b6fe5f54c1eb0291b00db867327edf91
tracer-ysyx <tracer@ysyx.org>
2023-12-23 17:24:53 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  17:24:53 up  1:17,  4 users,  load average: 0.00, 0.00, 0.01

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -56,19 +56,21 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  //printf("IN MAP_READ\n");
+  IFDEF(CONFIG_DTRACE,printf("<MAP-READ> Ready to read %d byte(s) for device(%s) at %#010x",len,map->name,addr));
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
+  IFDEF(CONFIG_DTRACE,printf("<MAP-READ> read " FMT_WORD " from device(%s)",ret,map->name));
   return ret;
 }
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  //printf("IN MAP_WRITE\n");
+  IFDEF(CONFIG_DTRACE,printf("<MAP-WRITE> Ready to write %d byte(s) for device(%s) at %#010x",len,map->name,addr));
   check_bound(map, addr);
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
+  IFDEF(CONFIG_DTRACE,printf("<MAP-WRITE> write " FMT_WORD " to device(%s)",data,map->name));
   invoke_callback(map->callback, offset, len, true);
 }
