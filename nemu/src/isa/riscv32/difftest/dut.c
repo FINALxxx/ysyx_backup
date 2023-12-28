c9dce7f6954fbe6323f0604c2a254193bd5943e1
tracer-ysyx <tracer@ysyx.org>
2023-12-28 20:39:31 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  20:39:31 up  1:28,  3 users,  load average: 0.00, 0.02, 0.00

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -22,7 +22,10 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	//dut用的CPU_state是extern的一个cpu，在isa.h中定义
 	uint32_t reg_len = sizeof(cpu.gpr)/sizeof(cpu.gpr[0]);
 	for(int i=0;i<reg_len;i++){
-		if(cpu.gpr[i]!=ref_r->gpr[i]) return false;
+		if(cpu.gpr[i]!=ref_r->gpr[i]) {
+			printf(ANSI_FMT("<DIFFTEST-%s>%x(IN dut:NEMU) != %x(IN ref:spike)\n",ANSI_FG_YELLOW),reg_name(i),cpu.gpr[i],ref_r->gpr[i]);
+			return false;
+		}
 	}
 	return true;
 }
