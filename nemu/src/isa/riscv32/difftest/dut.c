63a280dfda8e806f79b413389771e16b74c2283d
tracer-ysyx <tracer@ysyx.org>
2023-12-29 13:01:07 +0800

    > compile NEMU ysyx_23060094 严森 Linux FINALx 5.15.133.1-microsoft-standard-WSL2 #1 SMP Thu Oct 5 21:02:42 UTC 2023 x86_64 x86_64 x86_64 GNU/Linux  13:01:07 up  2:14,  3 users,  load average: 0.02, 0.02, 0.00

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -17,7 +17,9 @@
 #include <cpu/difftest.h>
 #include "../local-include/reg.h"
 
-#define DIFFER(csr) if(cpu.csr != ref_r->csr) return false;
+#define DIFFER(csr) if(cpu.csr != ref_r->csr){ \
+					printf(ANSI_FMT("<DIFFTEST-csr> %#010x(IN dut:NEMU) != %#010x(IN ref:spike)\n",ANSI_FG_YELLOW),cpu.csr,ref_r->csr);\
+					return false;}
 	
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 	//dut用的CPU_state是extern的一个cpu，在isa.h中定义
